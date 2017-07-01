9f7be5e8f183bb5f5ad263cb739b3134a14c5a6e
John Fansler <jfansler@vyasystems.com>
2017-06-30 23:08:18 -0700

    adding ether to server

--- server.php
+++ server.php
@@ -33,6 +33,17 @@ $server->loop->addPeriodicTimer(10, function() use ($app, &$coins){
 		}
 	}
 });
+$server->loop->addPeriodicTimer(10, function() use ($app, &$coins){
+	$ticker = json_decode(file_get_contents('https://etherlive.ethnews.com/api/v2/live?exchange=All&currency=USD'));
+	$usd = $ticker->price;
+	if ($coins['ether']!==$usd){
+		$coins['ether']=$usd;
+		$message = json_encode(['action'=>'ether', 'price'=>number_format($usd,2), 'error'=>false]);
+		foreach ($app->controller->clients as $conn){
+			$conn->send($message);
+		}
+	}
+});
 
 //run server
 $server->run();
