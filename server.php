6c3e7b6d45bda7ba22a4fc78e870af8db6f4adb4
John Fansler <jfansler@vyasystems.com>
2017-06-30 22:59:44 -0700

    Can't run two loops at once. Back to sharing a poll.

--- server.php
+++ server.php
@@ -21,38 +21,18 @@ $server = IoServer::factory(
 
 $coins = ['usd'=>0];
 
-$websocketClient = \Ratchet\Client\connect('wss://ws.pusherapp.com/app/de504dc5763aeef9ff52?protocol=7&client=js&version=2.1.6&flash=false')->then(function($conn) use ($app, &$coins, &$conn) {
-	$conn->on('message', function($msg) use ($conn, &$coins, $app) {
-		$msgObject = json_decode($msg);
-		var_dump($msgObject);
-		$usd = $msgObject->data->price;
-		if ($coins['usd']!==$usd){
-			$coins['usd']=$usd;
-			$message = json_encode(['action'=>'bitcoin', 'price'=>number_format($usd,2), 'error'=>false]);
-			foreach ($app->controller->clients as $serverConn){
-				$serverConn->send($message);
-			}
+
+$server->loop->addPeriodicTimer(10, function() use ($app, &$coins){
+	$ticker = json_decode(file_get_contents('https://blockchain.info/ticker'));
+	$usd = $ticker->USD->last;
+	if ($coins['usd']!==$usd){
+		$coins['usd']=$usd;
+		$message = json_encode(['action'=>'bitcoin', 'price'=>number_format($usd,2), 'error'=>false]);
+		foreach ($app->controller->clients as $conn){
+			$conn->send($message);
 		}
-	});
-	$conn->send('{"event":"pusher:subscribe","data":{"channel":"live_orders"}}');
-}, function ($e) {
-	echo "Could not connect: {$e->getMessage()}\n";
+	}
 });
-
-var_dump($websocketClient);
-
-
-//$server->loop->addPeriodicTimer(5, function() use ($app, &$coins){
-//	$ticker = json_decode(file_get_contents('https://blockchain.info/ticker'));
-//	$usd = $ticker->USD->last;
-//	if ($coins['usd']!==$usd){
-//		$coins['usd']=$usd;
-//		$message = json_encode(['action'=>'bitcoin', 'price'=>number_format($usd,2), 'error'=>false]);
-//		foreach ($app->controller->clients as $conn){
-//			$conn->send($message);
-//		}
-//	}
-//});
 
 //run server
 $server->run();
