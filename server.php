0ac5360f5adfcb2ab3019da4293831713e253295
John Fansler <jfansler@vyasystems.com>
2017-06-30 21:43:17 -0700

    trying a websocket client in the server

--- server.php
+++ server.php
@@ -21,17 +21,36 @@ $server = IoServer::factory(
 
 $coins = ['usd'=>0];
 
-$server->loop->addPeriodicTimer(5, function() use ($app, &$coins){
-	$ticker = json_decode(file_get_contents('https://blockchain.info/ticker'));
-	$usd = $ticker->USD->last;
-	if ($coins['usd']!==$usd){
-		$coins['usd']=$usd;
-		$message = json_encode(['action'=>'bitcoin', 'price'=>number_format($usd,2), 'error'=>false]);
-		foreach ($app->controller->clients as $conn){
-			$conn->send($message);
-		}
-	}
-});
+//$server->loop->addPeriodicTimer(5, function() use ($app, &$coins){
+//	$ticker = json_decode(file_get_contents('https://blockchain.info/ticker'));
+//	$usd = $ticker->USD->last;
+//	if ($coins['usd']!==$usd){
+//		$coins['usd']=$usd;
+//		$message = json_encode(['action'=>'bitcoin', 'price'=>number_format($usd,2), 'error'=>false]);
+//		foreach ($app->controller->clients as $conn){
+//			$conn->send($message);
+//		}
+//	}
+//});
 
 //run server
 $server->run();
+
+
+
+\Ratchet\Client\connect('wss://ws.pusherapp.com/app/de504dc5763aeef9ff52?protocol=7&client=js&version=2.1.6&flash=false')->then(function($conn) use ($app, &$coins, &$conn) {
+	$conn->on('message', function($msg) use ($conn, &$coins, $app) {
+		$msgObject = json_decode($msg);
+		$usd = $msgObject->data->price;
+		if ($coins['usd']!==$usd){
+			$coins['usd']=$usd;
+			$message = json_encode(['action'=>'bitcoin', 'price'=>number_format($usd,2), 'error'=>false]);
+			foreach ($app->controller->clients as $conn){
+				$conn->send($message);
+			}
+		}
+	});
+	$conn->send('{"event":"pusher:subscribe","data":{"channel":"live_orders"}}');
+}, function ($e) {
+	echo "Could not connect: {$e->getMessage()}\n";
+});
