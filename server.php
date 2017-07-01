98a2a5f289b9e6dfbd0e2850ffa785e88b1a1dff
John Fansler <jfansler@vyasystems.com>
2017-06-30 21:05:52 -0700

    Only send the update if there is something to update.

--- server.php
+++ server.php
@@ -19,12 +19,17 @@ $server = IoServer::factory(
 	8100
 );
 
-$server->loop->addPeriodicTimer(5, function() use ($app){
+$coins = ['usd'=>0];
+
+$server->loop->addPeriodicTimer(5, function() use ($app, $coins){
 	$ticker = json_decode(file_get_contents('https://blockchain.info/ticker'));
 	$usd = $ticker->USD->last;
-	$message = json_encode(['action'=>'bitcoin', 'price'=>number_format($usd,2), 'error'=>false]);
-	foreach ($app->controller->clients as $conn){
-		$conn->send($message);
+	if ($coins['usd']!==$usd){
+		$coins['usd']=$usd;
+		$message = json_encode(['action'=>'bitcoin', 'price'=>number_format($usd,2), 'error'=>false]);
+		foreach ($app->controller->clients as $conn){
+			$conn->send($message);
+		}
 	}
 });
 
