48ef96b9c0e946d5f6d7467722d4ddcf4d507f97
John Fansler <jfansler@vyasystems.com>
2017-06-30 22:38:50 -0700

    debugging 2

--- server.php
+++ server.php
@@ -21,23 +21,6 @@ $server = IoServer::factory(
 
 $coins = ['usd'=>0];
 
-//$server->loop->addPeriodicTimer(5, function() use ($app, &$coins){
-//	$ticker = json_decode(file_get_contents('https://blockchain.info/ticker'));
-//	$usd = $ticker->USD->last;
-//	if ($coins['usd']!==$usd){
-//		$coins['usd']=$usd;
-//		$message = json_encode(['action'=>'bitcoin', 'price'=>number_format($usd,2), 'error'=>false]);
-//		foreach ($app->controller->clients as $conn){
-//			$conn->send($message);
-//		}
-//	}
-//});
-
-//run server
-$server->run();
-
-
-
 $websocketClient = \Ratchet\Client\connect('wss://ws.pusherapp.com/app/de504dc5763aeef9ff52?protocol=7&client=js&version=2.1.6&flash=false')->then(function($conn) use ($app, &$coins, &$conn) {
 	$conn->on('message', function($msg) use ($conn, &$coins, $app) {
 		$msgObject = json_decode($msg);
@@ -57,3 +40,22 @@ $websocketClient = \Ratchet\Client\connect('wss://ws.pusherapp.com/app/de504dc57
 });
 
 var_dump($websocketClient);
+
+
+//$server->loop->addPeriodicTimer(5, function() use ($app, &$coins){
+//	$ticker = json_decode(file_get_contents('https://blockchain.info/ticker'));
+//	$usd = $ticker->USD->last;
+//	if ($coins['usd']!==$usd){
+//		$coins['usd']=$usd;
+//		$message = json_encode(['action'=>'bitcoin', 'price'=>number_format($usd,2), 'error'=>false]);
+//		foreach ($app->controller->clients as $conn){
+//			$conn->send($message);
+//		}
+//	}
+//});
+
+//run server
+$server->run();
+
+
+
