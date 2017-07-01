b90f9aa5240be9e75b18355426ddfcd8712e6477
John Fansler <jfansler@vyasystems.com>
2017-06-30 20:37:53 -0700

    Adding periodic timer

--- server.php
+++ server.php
@@ -7,15 +7,26 @@ use Ratchet\WebSocket\WsServer;
 //load composer
 require 'vendor/autoload.php';
 
+$app = new Router();
+
 //setup server
 $server = IoServer::factory(
 	new HttpServer(
 		new WsServer(
-			new Router(true)
+			$app
 		)
 	),
 	8100
 );
+
+$server->loop->addPeriodicTimer(5, function() use ($app){
+	$ticker = json_decode(file_get_contents('https://blockchain.info/ticker'));
+	$usd = $ticker->USD->last;
+	$message = json_encode(['action'=>'notification', 'message'=>number_format($usd,2), 'error'=>false]);
+	foreach ($app->controller->clients as $conn){
+		$conn->send($message);
+	}
+});
 
 //run server
 $server->run();
